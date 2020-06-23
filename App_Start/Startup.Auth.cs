be0305843e2c1c8426034259d3829ba833a2bd57
jjunno <joonas@koodattu.com>
2020-06-23 18:13:39 +0300

    Added error handling

--- App_Start/Startup.Auth.cs
+++ App_Start/Startup.Auth.cs
@@ -26,7 +26,7 @@ namespace RuuviTagApp
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Home/Index"),
+                LoginPath = new PathString("/Error/Rejected"),
                 Provider = new CookieAuthenticationProvider
                 {
                     // Enables the application to validate the security stamp when the user logs in.
